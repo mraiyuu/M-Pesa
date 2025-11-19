50394f15f0d38a12ab91c98f45ad961a6f339c0b
mraiyuu <munenedenis618@gmail.com>
2025-11-19 11:56:51 +0300

    server

---
+++ cmd/api.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"time"
+	"log"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID) 
+	r.Use(middleware.RealIP)    
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer) 
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good"))
+	})
+
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:              app.config.addr,
+		Handler:           h,
+		WriteTimeout:      time.Second * 30,
+		ReadHeaderTimeout: time.Second * 10,
+		IdleTimeout:       time.Minute,
+	}
+
+	log.Printf("server has started at addr %s", app.config.addr)
+	return srv.ListenAndServe()
+}
+
+type application struct {
+	config config
+	//logger
+	//db driver
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
